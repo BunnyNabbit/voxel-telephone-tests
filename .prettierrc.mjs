1260f803bff2d27f1455b2dd925291a9c30f77fc
BunnyNabbit (Aon) <aon@bunnynabbit.com>
2025-07-07 09:06:40 -0500

    Add Prettier config

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+	semi: false,
+	plugins: ["prettier-plugin-drone-class"],
+	printWidth: Infinity,
+	trailingComma: "es5",
+	useTabs: true,
+	endOfLine: "auto",
+}
